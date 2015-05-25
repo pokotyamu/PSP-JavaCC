b8e5852f6a79f7ff64747e7b697713c4ba44cc39
pokotyamu <pokotyamu@gmail.com>
2015-05-26 00:50:00 +0900

    change test dataform DataSet to SeriesData

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import Graphbox.SeriesParser;
 import java.io.*;
 import parse.*;
 import pspdata.*;
@@ -6,7 +7,10 @@ import box.*;
 public class Main{
     public static void main(String[] args) throws IOException{
         Contents c = ProcessParser.parse("/Users/pokotyamu/NetBeansProjects/PSP/src/PSP.txt");
-        Calculation.cal(0,c).debugPrint();
+        Calculation.cal(0, c).debugPrint();
+        
+        SeriesParser sp = new SeriesParser();
+        sp.parse(Calculation.cal(0,c)).debugPrint();
     }
     
 }
